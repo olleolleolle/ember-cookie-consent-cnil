f0126b3e3a8609d829bccb4e988fa07678e4e820
Charles Duval <charles.duval@infoduv.fr>
2015-10-20 17:30:09 +0200

    correct bower dependency installation

--- blueprints/ember-cookie-consent-cnil/index.js
+++ blueprints/ember-cookie-consent-cnil/index.js
@@ -1,7 +1,10 @@
 'use strict';
 
 module.exports = {
-  description: '',
+  description: 'Adding jquery.cookie bower package dependency',
+  
+    normalizeEntityName: function() {}, // no-op since we're just adding dependencies
+
 
   afterInstall: function(options) {
     return this.addBowerPackageToProject('jquery.cookie');
